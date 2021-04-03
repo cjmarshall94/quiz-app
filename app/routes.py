1f2cf43a4dc3f3f5131c72617b435cab9086c2a8
Chris Marshall <chrismarshall1194@gmail.com>
2021-04-03 18:18:12 +0100

    Questions and results page now using DB rather than static dict

--- app/routes.py
+++ app/routes.py
@@ -4,87 +4,28 @@ import random, copy
 from app import app, db
 from app.models import Question
 
-original_questions_answers = {
-    'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona':'Phoenix', 'Arkansas':'Little Rock',
-    'California': 'Sacramento', 'Colorado':'Denver', 'Connecticut':'Hartford', 'Delaware':'Dover',
-    'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
-    'Illinios': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Monies', 'Kansas': 'Topeka',
-    'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
-    'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'St. Paul', 'Mississippi': 'Jackson',
-    'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City',
-    'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany',
-    'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
-    'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City',
-    'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
-    'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'
-}
-
-# get a list of the answers
-questions_answers = copy.deepcopy(original_questions_answers)
-
-# build the list of the correct answers to compare (in global scope)
-correct_answers = []
-user_answers = []
-
-# initialising the score variable
-score = []
-
+# Main quiz page
 @app.route("/", methods=["GET", "POST"])
 def quiz():
 
+	questions = Question.query.all()
+	# Get 5 random questions
+	quiz_list = random.sample(questions, 5)
+
 	if request.method == "GET":
+		return render_template("main.html", questions=quiz_list)
 
-		# load up a random selection of 5 dictionary keys each time
-		randomiser = random.sample(range(1, len(questions_answers)), 5)
-		print(randomiser)
-
-		correct_answers.clear()
-
-		for num in randomiser:
-			correct_answers.append(list(questions_answers.values())[num])
-		print(correct_answers)
-
-		# render html page
-		return render_template("main.html", q=list(questions_answers.keys()), a=list(questions_answers.values()), rand=randomiser)
-
-	# logic for after form submitted (POST request)
 	else:
-
-		# get the data from the form
-		req = request.form
-
-		# clear user answers from previous attempt
-		user_answers.clear()
-
-		# push the data into a format that I can use to compare with correct answers
-		for i in list(req.values()):
-			user_answers.append(i)
-		print(user_answers)
-
-		# count the score
-		list_matching_answers = [i for i, j in zip(user_answers, correct_answers) if i == j]
-
-		# clear score from previous attempt
-		score.clear()
-
-		for i in list_matching_answers:
-			score.append(i)
-
-		# give the user an alert saying what they scored
-		print(f"You scored {len(score)} out of 5!")
-
-		# redirect to the route page after alert and form submission
-		return redirect(url_for("show_result"))
-		
+		user_answers = request.form.getlist("answer-box")
+		return render_template("results.html", questions=quiz_list, user_answers=user_answers)
 
 
+# Page to show correct / wrong answers to the quiz
 @app.route("/results", methods=["GET", "POST"])
 def show_result():
 
 	if request.method == "GET":
-		return render_template("results.html", score=score, correct_answers=correct_answers, user_answers=user_answers, original_questions_answers=original_questions_answers)
-	return redirect(url_for("quiz"))
+		return render_template("results.html")
 
 
 # This page lets you add a question to the quiz
@@ -109,6 +50,7 @@ def success():
 
 	questions = Question.query.all()
 	return render_template("success.html", questions=questions)
+
 
 # Ability to remove questions from the quiz
 @app.route("/delete", methods=["POST"])
