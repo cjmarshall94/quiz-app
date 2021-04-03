5574a065cdfe5fd951d7aaac428192504f30a1ce
Chris Marshall <chrismarshall1194@gmail.com>
2021-04-03 21:55:03 +0100

    Added results page with correct answer comparison

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,53 @@
 from flask import render_template, request, url_for, redirect
-import random, copy
+import random, copy, collections
+import pandas as pd
 
 from app import app, db
 from app.models import Question
 
 # Main quiz page
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def quiz():
 
 	questions = Question.query.all()
+
 	# Get 5 random questions
 	quiz_list = random.sample(questions, 5)
 
-	if request.method == "GET":
-		return render_template("main.html", questions=quiz_list)
+	ordered_quiz_list = sorted(quiz_list, key=lambda x: x.id)
 
-	else:
-		user_answers = request.form.getlist("answer-box")
-		return render_template("results.html", questions=quiz_list, user_answers=user_answers)
+	return render_template("main.html", questions=ordered_quiz_list)
 
 
 # Page to show correct / wrong answers to the quiz
-@app.route("/results", methods=["GET", "POST"])
+@app.route("/results", methods=["POST"])
 def show_result():
 
-	if request.method == "GET":
-		return render_template("results.html")
+	if request.method == "POST":
+		
+		# Record the user's answers alongside the question that was asked for that answer
+		user_answers = {}
+		for key, value in request.form.items():
+			user_answers[key] = value
+
+		# Order by the question ID
+		ordered_answers = collections.OrderedDict(sorted(user_answers.items()))
+
+		# Get the relevant question based on the ID (because want to render out the correct Q/As alongside user's answers)
+		question_ids = ordered_answers.keys()
+		ordered_quiz_list = Question.query.filter(Question.id.in_(question_ids)).all()
+
+		# We just need the ordered list of values for the front end
+		ordered_answers = ordered_answers.values()
+
+		# Create the results table
+		results_table = pd.DataFrame(data={
+										'Question': [question.question for question in ordered_quiz_list], 
+										'Correct Answer': [question.answer for question in ordered_quiz_list], 
+										'Your Answer': ordered_answers
+										})
+
+		return render_template("results.html", tables=[results_table.to_html()], titles=results_table.columns.values)
 
 
 # This page lets you add a question to the quiz
